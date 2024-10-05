830bea7c74d64cfa11545a9bec74683734c6e3be
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-10-05 12:51:29 +0000

    Ajout d'un proxy pour tous les requêtes vers l

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3003
+    port: 3003,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8084',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
