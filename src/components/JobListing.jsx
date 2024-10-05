76959a5dceb3829b518cf315750e1ba3b190206f
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-10-05 10:41:15 +0000

    Ajout du routage et d'un Layout

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,16 +1,31 @@
+import {useState} from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 const JobListing = ({job}) => {
+    const [showFullDescription,setShowFullDescription ] = useState(false);
+
+    let description = job.description;
+
+
+
+    if(!showFullDescription){
+        description = description.substring(0, 90) + "...";
+    }
   return (
       <div className="bg-white p-6 rounded-lg shadow-md">
           <p className="text-indigo-600 font-bold text-sm mb-2">{job.type}</p>
           <h3 className="text-lg font-bold mb-2">{job.title}</h3>
           <p className="text-gray-600 mb-4">
-              {job.description}
+              {description}
           </p>
-          <a href="#" className="text-indigo-600 font-bold mb-2 block">More</a>
+          <button onClick={() => setShowFullDescription(prevState => !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600 hover:font-bold">
+              {showFullDescription ? 'Less' : 'More'}
+          </button>
+
+          {/*<a href="#" className="text-indigo-600 font-bold mb-2 block">More</a>*/}
           <p className="text-gray-800 font-bold">{job.salary} / Year</p>
           <p className="text-gray-600">
-              <i className="fas fa-map-marker-alt text-red-500"></i> {job.location}
+              <FaMapMarker className="inline text-lg mb-1 text-red-600"/> {job.location}
           </p>
           <a href={`/jobs/${job.id}`} className="mt-4 inline-block bg-indigo-600 text-white px-4 py-2 rounded">Read
               More</a>
