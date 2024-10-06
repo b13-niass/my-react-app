0e4a734d3823f77db9e4fdf847fe031fc6f3ba78
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-10-06 14:53:53 +0000

    utilisation de Formik pour le formulaire d'ajout avec validation

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,56 +1,56 @@
-import {useState} from "react";
 import {useNavigate} from "react-router-dom";
+import {useFormik} from "formik";
+import {toast} from "react-toastify";
+import * as Yup from "yup";
 
 const AddJobPage = ({addJobSubmit}) => {
-   const [type,setType] = useState("Full-Time");
-   const [title, setTitle] = useState("");
-  const [location, setLocation] = useState('');
-   const [description, setDescription] = useState('');
-   const [salary, setSalary] = useState('Under $50K');
-   const [companyName, setCompanyName] = useState('');
-   const [companyDescription, setCompanyDescription] = useState('');
-   const [contactEmail, setContactEmail] = useState('');
-   const [contactPhone, setContactPhone] = useState('');
+    const navigate = useNavigate();
 
-   const navigate = useNavigate();
+    const validationSchemaYup = Yup.object().shape({
+        type: Yup.string().required("Job Type is required"),
+        title: Yup.string().required("Job Listing Name is required"),
+        location: Yup.string().required("Location is required"),
+        description: Yup.string().required("Description is required"),
+        salary: Yup.string().required("Salary is required"),
+        company: Yup.object().shape({
+            name: Yup.string().required("Company Name is required"),
+            description: Yup.string().required("Company Description is required"),
+            contactEmail: Yup.string().email("Invalid email format").required("Contact Email is required"),
+            contactPhone: Yup.string().required("Le numéro de téléphone est requis").matches(
+                /^(77|78|76)\d{7}$/,
+                "Invalid phone number format (77|78|76)7777777"
+            )
+        })
+    })
 
-   const submitForm = (e) => {
-       e.preventDefault();
+    const formik = useFormik({
+        initialValues: {
+            type: "Full-Time",
+            title: "",
+            location: "",
+            description: "",
+            salary: "Under $50K",
+            company:{
+                name: "",
+                description: "",
+                contactEmail: "",
+                contactPhone: ""
+            }
+        },
+        onSubmit: (values) => {
+            addJobSubmit(values);
+            toast.success("Job added successfully");
+            navigate("/jobs");
+        },
+        validationSchema: validationSchemaYup
+    })
 
-       const newJob ={
-           type,
-           title,
-           location,
-           description,
-           salary,
-           company: {
-               name: companyName,
-               description: companyDescription,
-               contactEmail,
-               contactPhone
-           }
-       };
-       const result = addJobSubmit(newJob);
-       console.log(result);
-       // Reset form fields
-       setType("Full-Time");
-       setTitle("");
-       setLocation("");
-       setDescription("");
-       setSalary("");
-       setCompanyName("");
-       setCompanyDescription("");
-       setContactEmail("");
-       setContactPhone("");
-
-       return navigate('/jobs');
-   }
 
     return (
         <section className='bg-indigo-50'>
             <div className='container m-auto max-w-2xl py-24'>
                 <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
-                    <form onSubmit={submitForm}>
+                    <form onSubmit={formik.handleSubmit}>
                         <h2 className='text-3xl text-center font-semibold mb-6'>Add Job</h2>
 
                         <div className='mb-4'>
@@ -64,17 +64,19 @@ const AddJobPage = ({addJobSubmit}) => {
                                 id='type'
                                 name='type'
                                 className='border rounded w-full py-2 px-3'
-                                required
-                                value={type}
-                                onChange={(e) => setType(e.target.value)}
+                                value={formik.values.type}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             >
                                 <option value='Full-Time'>Full-Time</option>
                                 <option value='Part-Time'>Part-Time</option>
                                 <option value='Remote'>Remote</option>
                                 <option value='Internship'>Internship</option>
                             </select>
+                            {formik.touched.type && formik.errors.type && (
+                                <p className='text-red-500'>{formik.errors.type}</p>
+                            )}
                         </div>
-
                         <div className='mb-4'>
                             <label className='block text-gray-700 font-bold mb-2'>
                                 Job Listing Name
@@ -85,10 +87,13 @@ const AddJobPage = ({addJobSubmit}) => {
                                 name='title'
                                 className='border rounded w-full py-2 px-3 mb-2'
                                 placeholder='eg. Beautiful Apartment In Miami'
-                                required
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
+                                value={formik.values.title}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.type && formik.errors.title && (
+                                <p className='text-red-500'>{formik.errors.title}</p>
+                            )}
                         </div>
                         <div className='mb-4'>
                             <label
@@ -103,11 +108,14 @@ const AddJobPage = ({addJobSubmit}) => {
                                 className='border rounded w-full py-2 px-3'
                                 rows='4'
                                 placeholder='Add any job duties, expectations, requirements, etc'
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
+                                value={formik.values.description}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             ></textarea>
+                            {formik.touched.type && formik.errors.description && (
+                                <p className='text-red-500'>{formik.errors.description}</p>
+                            )}
                         </div>
-
                         <div className='mb-4'>
                             <label
                                 htmlFor='type'
@@ -119,9 +127,9 @@ const AddJobPage = ({addJobSubmit}) => {
                                 id='salary'
                                 name='salary'
                                 className='border rounded w-full py-2 px-3'
-                                required
-                                value={salary}
-                                onChange={(e) => setSalary(e.target.value)}
+                                value={formik.values.salary}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             >
                                 <option value='Under $50K'>Under $50K</option>
                                 <option value='$50K - 60K'>$50K - $60K</option>
@@ -135,8 +143,10 @@ const AddJobPage = ({addJobSubmit}) => {
                                 <option value='$175K - 200K'>$175K - $200K</option>
                                 <option value='Over $200K'>Over $200K</option>
                             </select>
+                            {formik.touched.type && formik.errors.salary && (
+                                <p className='text-red-500'>{formik.errors.salary}</p>
+                            )}
                         </div>
-
                         <div className='mb-4'>
                             <label className='block text-gray-700 font-bold mb-2'>
                                 Location
@@ -147,12 +157,14 @@ const AddJobPage = ({addJobSubmit}) => {
                                 name='location'
                                 className='border rounded w-full py-2 px-3 mb-2'
                                 placeholder='Company Location'
-                                required
-                                value={location}
-                                onChange={(e) => setLocation(e.target.value)}
+                                value={formik.values.location}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.type && formik.errors.location && (
+                                <p className='text-red-500'>{formik.errors.location}</p>
+                            )}
                         </div>
-
                         <h3 className='text-2xl mb-5'>Company Info</h3>
 
                         <div className='mb-4'>
@@ -165,14 +177,17 @@ const AddJobPage = ({addJobSubmit}) => {
                             <input
                                 type='text'
                                 id='company'
-                                name='company'
+                                name='company[name]'
                                 className='border rounded w-full py-2 px-3'
                                 placeholder='Company Name'
-                                value={companyName}
-                                onChange={(e) => setCompanyName(e.target.value)}
+                                value={formik.values.company.name}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.type && formik.errors.company.name && (
+                                <p className='text-red-500'>{formik.errors.company.name}</p>
+                            )}
                         </div>
-
                         <div className='mb-4'>
                             <label
                                 htmlFor='company_description'
@@ -182,15 +197,18 @@ const AddJobPage = ({addJobSubmit}) => {
                             </label>
                             <textarea
                                 id='company_description'
-                                name='company_description'
+                                name='company[description]'
                                 className='border rounded w-full py-2 px-3'
                                 rows='4'
                                 placeholder='What does your company do?'
-                                value={companyDescription}
-                                onChange={(e) => setCompanyDescription(e.target.value)}
+                                value={formik.values.company.description}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             ></textarea>
+                            {formik.touched.type && formik.errors.company.description && (
+                                <p className='text-red-500'>{formik.errors.company.description}</p>
+                            )}
                         </div>
-
                         <div className='mb-4'>
                             <label
                                 htmlFor='contact_email'
@@ -201,13 +219,16 @@ const AddJobPage = ({addJobSubmit}) => {
                             <input
                                 type='email'
                                 id='contact_email'
-                                name='contact_email'
+                                name='company[contactEmail]'
                                 className='border rounded w-full py-2 px-3'
                                 placeholder='Email address for applicants'
-                                required
-                                value={contactEmail}
-                                onChange={(e) => setContactEmail(e.target.value)}
+                                value={formik.values.company.contactEmail}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.type && formik.errors.company.contactEmail && (
+                                <p className='text-red-500'>{formik.errors.company.contactEmail}</p>
+                            )}
                         </div>
                         <div className='mb-4'>
                             <label
@@ -219,12 +240,16 @@ const AddJobPage = ({addJobSubmit}) => {
                             <input
                                 type='tel'
                                 id='contact_phone'
-                                name='contact_phone'
+                                name='company[contactPhone]'
                                 className='border rounded w-full py-2 px-3'
                                 placeholder='Optional phone for applicants'
-                                value={contactPhone}
-                                onChange={(e) => setContactPhone(e.target.value)}
+                                value={formik.values.company.contactPhone}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.type && formik.errors.company.contactPhone && (
+                                <p className='text-red-500'>{formik.errors.company.contactPhone}</p>
+                            )}
                         </div>
 
                         <div>
