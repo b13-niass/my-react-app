33591e97c99be0c00901eb2d635ade7725f2ab91
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-10-06 10:26:43 +0000

    Ajout détail job

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,33 +1,30 @@
-import {useState, useEffect} from "react";
 import JobListing from "./JobListing";
 import Spinner from "./Spinner.jsx";
-const JobListings = ({isHome = false}) => {
-    const [jobs, setJobs] = useState([]);
-    const [loading, setLoading] = useState(true);
+import {useQuery} from "@tanstack/react-query";
 
-    useEffect(() => {
-        const fetchJobs = async () => {
-            const apiUrl = isHome ? "/api/jobs?_limit=3" :"/api/jobs";
-          try {
-              const res = await fetch(apiUrl);
-              const data = await res.json();
-              setJobs(data);
-          }catch (err) {
-              console.error('Error fetching jobs:', err);
-          }finally {
-              setLoading(false);
-          }
+const JobListings = ({isHome = false}) => {
+    const getJobs = async () => {
+        const apiUrl = isHome ? "/api/jobs?_limit=3" :"/api/jobs";
+        try {
+            const res = await fetch(apiUrl);
+            return  await res.json();
+        }catch (err) {
+            console.error('Error fetching jobs:', err);
         }
-        fetchJobs();
-    }, [])
+    }
+
+    const { data: jobs, status, error} = useQuery({
+        queryKey:["jobs"],
+        queryFn: getJobs
+    })
 
   return (
       <section className="bg-blue-50 py-10">
           <div className="container mx-auto">
               <h2 className="text-2xl font-bold text-center mb-6">{isHome ? 'Recent Jobs' : 'Browse Jobs'}</h2>
-                  <Spinner loading={loading}/>
+                  <Spinner loading={status === "pending"}/>
               <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                  {!loading && jobs.map((job) => (
+                  {status !== "pending" && jobs.map((job) => (
                      <JobListing key={job.id} job={job}/>
                   ))}
               </div>
